61052a111631b3b2c1e7de13fd25c95ccd373e92
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-22 12:46:23 +0200

    Inicial commit

---
+++ src/test/java/com/example/api/Ud2602ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ud2602ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
