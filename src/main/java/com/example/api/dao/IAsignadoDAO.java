9c9b3623342fa423f5948dee6eb29dceeb924c32
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-22 17:18:48 +0200

    DAO

---
+++ src/main/java/com/example/api/dao/IAsignadoDAO.java
@@ -0,0 +1,9 @@
+package com.example.api.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.api.dto.Asignado;
+
+public interface IAsignadoDAO extends JpaRepository<Asignado, Integer>{
+
+}
---
+++ src/main/java/com/example/api/dao/ICientificoDAO.java
@@ -0,0 +1,9 @@
+package com.example.api.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.api.dto.Cientifico;
+
+public interface ICientificoDAO extends JpaRepository<Cientifico, String>{
+
+}
